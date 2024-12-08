d387cc2ac7c5b210efb8e8ce7d1fe7dfa28fc83c
Logan Oram <logan.oram@keyin.com>
2024-12-07 22:17:13 -0330

    inserted sample data

--- src/resources/sample_data.sql
+++ src/resources/sample_data.sql
@@ -1,1 +1,6 @@
-SELECT * FROM users;
+-- Insert Sample Data For Users
+INSERT INTO users (username, password,email,role)
+VALUES
+('Logan', 'password1', 'logan@gmail.com', 'admin'),
+('Declan', 'password2', 'declan@gmail.com', 'buyer'),
+('Nick', 'password3', 'nick@gmail.com', 'seller');
