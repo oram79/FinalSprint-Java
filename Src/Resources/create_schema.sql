958c6684d77e0de1fab8ff5952cce4a8f684fd9b
Logan Oram <logan.oram@keyin.com>
2024-12-08 17:43:46 -0330

    added query

--- Src/Resources/create_schema.sql
+++ Src/Resources/create_schema.sql
@@ -1,3 +1,4 @@
+-- User Table --
 CREATE TABLE IF NOT EXISTS Users(
  id SERIAL PRIMARY KEY,
  username VARCHAR(50) NOT NULL UNIQUE,
@@ -5,11 +6,18 @@ CREATE TABLE IF NOT EXISTS Users(
  email VARCHAR(100) NOT NULL UNIQUE,
  role VARCHAR(10) NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS Products (
-id SERIAL PRIMARY KEY,
-name VARCHAR(50) NOT NULL UNIQUE,
-password VARCHAR(225) NOT NULL,
-email VARCHAR(100) NOT NULL UNIQUE,
-role VARCHAR(10) NOT NULL
+-- Products Table --
+CREATE TABLE Products (
+ id SERIAL PRIMARY KEY,
+ name VARCHAR(100) NOT NULL,
+ price DECIMAL(10, 2) NOT NULL,
+ quantity INT NOT NULL,
+ seller_id INT NOT NULL REFERENCES Users(id)
 );
+
+-- Authenticating A User --
+SELECT * FROM users
+WHERE username = 'Logan' AND password = 'password1';
+
+-- Display All Active Products --
+SELECT * FROM products;
