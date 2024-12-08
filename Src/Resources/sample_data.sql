745c75fbff9b67a43bf3709dc2f9d92a80b8679f
Logan Oram <logan.oram@keyin.com>
2024-12-08 17:27:39 -0330

    sample data added

---
+++ Src/Resources/sample_data.sql
@@ -0,0 +1,13 @@
+-- Insert Users
+INSERT INTO Users (username, password, email, role)
+VALUES
+('Logan', 'password1', 'logan@gmail.com', 'admin'),
+('Declan', 'password2', 'declan@gmail.com', 'buyer'),
+('Nick', 'password3', 'nick@gmail.com', 'seller');
+
+-- Insert Products
+INSERT INTO Products (name, price, quantity, seller_id)
+VALUES
+('Product 1', 100.00, 10, 2),
+('Product 2', 50.00, 5, 2),
+('Product 3', 75.00, 20, 2);
