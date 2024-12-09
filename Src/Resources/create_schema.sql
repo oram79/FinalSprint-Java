699f2125e8347076d361e57ee83edf00b5717f8c
Logan Oram <logan.oram@keyin.com>
2024-12-09 10:33:05 -0330

    more query's added

--- Src/Resources/create_schema.sql
+++ Src/Resources/create_schema.sql
@@ -21,3 +21,22 @@ WHERE username = 'Logan' AND password = 'password1';
 
 -- Display All Active Products --
 SELECT * FROM products;
+
+-- List Products by a Seller --
+SELECT * FROM Products
+WHERE seller_id = (SELECT id FROM Users WHERE username = 'Declan');
+
+-- Add A New Product --
+INSERT INTO Products (name, price, quantity, seller_id)
+VALUES ('Gaming PC', 1450.00, 5, 4);
+
+-- Updating Certain Products --
+UPDATE Products
+SET price = 699.99, quantity = 12
+WHERE id = 4 AND seller_id = (SELECT id FROM Users WHERE username = 'Declan');
+
+-- Delete A Product -- 
+DELETE FROM Products
+WHERE id = 2 AND seller_id = (SELECT id FROM Users WHERE username = 'Declan');
+
+
