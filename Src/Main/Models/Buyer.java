29280ce9f8bf95bd9845f8a77639b03d181c3be6
nsharpe1 <162132214+nsharpe1@users.noreply.github.com>
2024-12-11 18:19:50 -0330

    Update Buyer.java

--- Src/Main/Models/Buyer.java
+++ Src/Main/Models/Buyer.java
@@ -1,5 +1,7 @@
 package Src.Main.Models;
 
-public class Buyer {
-    
+public class Buyer extends User {
+    public Buyer(int ID, String username, String password, String email, String role) {
+        super(ID, username, password, email, role);
+    }
 }
