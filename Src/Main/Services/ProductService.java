470855ee990328ceba531faf6aef525c5aadedd0
Declan <declan.derible@keyin.com>
2024-12-11 15:11:18 -0330

    SQL throws and minor text fixes

--- Src/Main/Services/ProductService.java
+++ Src/Main/Services/ProductService.java
@@ -7,53 +7,37 @@ import Src.Main.Dao.ProductDAO;
 import Src.Main.Models.Product;
 
 public class ProductService {
-    private final productDAO productDAO;
+    private final ProductDAO productDAO;
 
     public ProductService(ProductDAO productDAO) {
         this.productDAO = productDAO;
     }
 
-    public void addProduct(int id, String name, double price, int quantity, int seller_id){
+    public void addProduct(int id, String name, double price, int quantity, int seller_id) throws SQLException{
         Product product = new Product(id, name, price, quantity, seller_id);
-        try {
-            productDAO.addProduct(product);
-            System.out.println("Product Listed!");
-        } catch (SQLException e) {
-            System.err.println("ERROR: Product Couldn't be Listed.");
-        }
+        productDAO.createProduct(product);
+        System.out.println("Product Listed!");
     }
 
-    public void removeProduct(int id){
-        try {
-            productDAO.deleteProduct(id);
-            System.out.println("Product Removed!");
-        } catch (SQLException e) {
-            System.err.println("ERROR: Product Couldn't be Removed.");
-        }
+    public void removeProduct(int id) throws SQLException{
+        productDAO.deleteProduct(id);
+        System.out.println("Product Removed!");
     }
 
-    public void updateProduct(int id, String name, double price, int quantity, int seller_id){
+    public void updateProduct(int id, String name, double price, int quantity, int seller_id) throws SQLException{
         Product product = new Product(id, name, price, quantity, seller_id);
-        try {
-            productDAO.updateProduct(product);
-            System.out.println("Product Updated!");
-        } catch (SQLException e) {
-            System.err.println("ERROR: Product Couldn't be Updated.");
-        }
+        productDAO.updateProduct(product);
+        System.out.println("Product Updated!");
     }
 
-    public void viewProduct(int id, String name, double price, int quantity, int seller_id){
-        try {
-            List<Product> products = productDAO.getAllProducts();
-            System.out.println("LISTED PRODUCTS:");
-            for (Product product : products) {
-                System.out.println("~~~~~~~~~~~");
-                System.out.println("Name: " + product.getName());
-                System.out.println("Price: $" + product.getPrice());
-                System.out.println("Quantity: " + product.getQuantity());
-            }
-        } catch (SQLException e) {
-            System.err.println("ERROR: Products Couldn't be Listed.");
+    public void viewProduct(int id, String name, double price, int quantity, int seller_id) throws SQLException{
+        List<Product> products = productDAO.getAllProducts();
+        System.out.println("LISTED PRODUCTS:");
+        for (Product product : products) {
+            System.out.println("~~~~~~~~~~~");
+            System.out.println("Name: " + product.getName());
+            System.out.println("Price: $" + product.getPrice());
+            System.out.println("Quantity: " + product.getQuantity());
         }
     }
 }
